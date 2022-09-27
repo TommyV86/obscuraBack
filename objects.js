40d494e0075f737f31b72a921e1ec0056aa3a419
Tommy Vichidvongsa <tommychel@hotmail.fr>
2022-09-27 11:30:53 +0200

    delete node folder and one file

--- objects.js
+++
@@ -1,7 +0,0 @@
-const copDbUri = 
-"mongodb+srv://karaCop:Aphisit86@copcluster.c6wfh.mongodb.net/projectDb?authSource=admin&replicaSet=atlas-f5itvv-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
-
-const admin = 1;
-const usualUser = 2;
-
-module.exports = { copDbUri, admin, usualUser };
